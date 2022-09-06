59283158c19d20bd30186032b24e8666a6ec41bd
sholabioku <bilushonline@gmail.com>
2022-09-06 18:32:15 +0100

    Add style.css file

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const colors = require('colors');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3500;
+
+app.use('/', express.static(path.join(__dirname, '/public')));
 
 app.listen(PORT, () =>
   console.log(`server running on port ${PORT}`.yellow.bold)
