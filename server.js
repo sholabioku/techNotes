5f9860625771a364a53121f81fad9cd36a108832
sholabioku <bilushonline@gmail.com>
2022-09-07 09:29:17 +0100

    Add functionlaity for database connection

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const colors = require('colors');
 const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
-const { logger } = require('./middleware/logger');
+const { logger, logEvents } = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
 const corsOptions = require('./config/corsOptions');
+const connectDB = require('./config/dbConn');
 
 const PORT = process.env.PORT || 3500;
+
+connectDB();
 
 app.use(logger);
 
@@ -35,6 +39,15 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () =>
-  console.log(`server running on port ${PORT}`.yellow.bold)
-);
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    'mongoErrLog.log'
+  );
+});
