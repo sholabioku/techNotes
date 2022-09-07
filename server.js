b06cc63ef8fdc066686c514bf43e7693f35e0de1
sholabioku <bilushonline@gmail.com>
2022-09-07 07:36:38 +0100

    Add helper logger middleware function an its functionality

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const colors = require('colors');
 const app = express();
 const path = require('path');
+const { logger } = require('./middleware/logger');
 const PORT = process.env.PORT || 3500;
+
+app.use(logger);
 
 app.use(express.json());
 
