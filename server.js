820b3434f5de7cdaab1be96b7df3271cfe10830e
sholabioku <bilushonline@gmail.com>
2022-09-07 07:43:39 +0100

    Add errorHandler middleware functionality and use it

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const colors = require('colors');
 const app = express();
 const path = require('path');
+
 const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
@@ -23,6 +26,8 @@ app.all('*', (req, res) => {
     res.type('txt').send('404 Not Found');
   }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () =>
   console.log(`server running on port ${PORT}`.yellow.bold)
