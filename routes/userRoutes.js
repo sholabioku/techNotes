9b171a5f8e6f8cdcaa49426649268de4435328f5
sholabioku <bilushonline@gmail.com>
2022-09-08 07:47:09 +0100

    Add controller and route setup for user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const {
+  getAllUsers,
+  createNewUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/usersController');
 const router = express.Router();
 
-router.route('/').get().post().patch().delete();
+router
+  .route('/')
+  .get(getAllUsers)
+  .post(createNewUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 module.exports = router;
