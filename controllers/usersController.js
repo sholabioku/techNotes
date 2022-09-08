353a57d695755bef198af00290d72e94927b6733
sholabioku <bilushonline@gmail.com>
2022-09-08 07:55:07 +0100

    Add functionality for GET /users to getAllUsers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,7 +6,12 @@ const bcrypt = require('bcrypt');
 // @desc Get all users
 // @route GET /users
 // @access Private
-const getAllUsers = asyncHandler(async (req, res) => {});
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find().select('-password').lean();
+  if (!users) return res.status(400).json({ message: 'No users found' });
+
+  res.status(200).json(users);
+});
 
 // @desc Create new user
 // @route POST /users
