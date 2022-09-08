a73bf7a0b587da6b134c5831e2a74ea592999f18
sholabioku <bilushonline@gmail.com>
2022-09-08 09:57:18 +0100

    Add functionality for DELETE /users to deleteUser method

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -74,6 +74,22 @@ const updateUser = asyncHandler(async (req, res) => {
 // @desc Delete a user
 // @route DELETE /users
 // @access Private
-const deleteUser = asyncHandler(async (req, res) => {});
+const deleteUser = asyncHandler(async (req, res) => {
+  const { id } = req.body;
+
+  if (!id) return res.status(400).json({ message: 'User ID is required' });
+
+  const notes = await Note.findOne({ user: id }).lean().exec();
+  if (notes?.length)
+    return res.status(200).json({ message: 'User has assigned notes' });
+
+  const user = await User.findById(id).exec();
+  if (!user) return res.status(400).json({ message: 'User not found' });
+
+  const result = await user.deleteOne();
+  const reply = `Username ${result.username} with ID ${result._id} deleted `;
+
+  res.status(200).json(reply);
+});
 
 module.exports = { getAllUsers, createNewUser, updateUser, deleteUser };
