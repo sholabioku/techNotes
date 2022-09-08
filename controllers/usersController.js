6578282b705b9273385c2f57353545e101ec610a
sholabioku <bilushonline@gmail.com>
2022-09-08 15:33:55 +0100

    Add a fix for user lenght

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -8,7 +8,8 @@ const bcrypt = require('bcrypt');
 // @access Private
 const getAllUsers = asyncHandler(async (req, res) => {
   const users = await User.find().select('-password').lean();
-  if (!users) return res.status(400).json({ message: 'No users found' });
+  if (!users?.length)
+    return res.status(400).json({ message: 'No users found' });
 
   res.status(200).json(users);
 });
